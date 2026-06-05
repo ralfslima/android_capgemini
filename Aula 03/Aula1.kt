271b4cb8d17ee7426ef4624ac4f6f75fcff6a0dd
ralfslima <62228067+ralfslima@users.noreply.github.com>
2026-06-05 07:24:40 -0300

    03 de junho de 2026

---
+++ Aula 03/Aula1.kt
@@ -0,0 +1,32 @@
+package br.com.treinamento.nivelamento
+
+// Estrutura de função
+fun main(){
+
+    // *******************************************
+    // Elvis Operator
+    var nomeUsuario: String? = null
+    var nome: String = nomeUsuario ?: "Visitante"
+    print(nome)
+
+    // *******************************************
+    // Constantes
+    //val dolar: Double = 5.10
+
+
+    // *******************************************
+    // Variáveis
+    //var nome: String = "Ralf"
+    //var idade: Int = 36
+    //var cidade: String? = null  //<- Null Safety
+
+    //println("Olá " + nome + ", você tem " + idade + " anos.") // <- Não usual
+    //println("Olá $nome, você tem $idade anos.")  // <- Usual
+
+
+    // *******************************************
+    // Exibir mensagem
+    //print("Hello World!")
+
+}
+
