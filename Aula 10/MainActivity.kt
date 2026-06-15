c59a19559cea85bdc2ff6bd48ec1cdcddfce6303
ralfslima <62228067+ralfslima@users.noreply.github.com>
2026-06-15 20:25:28 -0300

    15 de junho de 2026

---
+++ Aula 10/MainActivity.kt
@@ -0,0 +1,43 @@
+package br.com.treinamento.modulojetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.treinamento.modulojetpackcompose.componentes.Componente01_Box
+import br.com.treinamento.modulojetpackcompose.ui.theme.ModuloJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ModuloJetpackComposeTheme {
+                Componente01_Box()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ModuloJetpackComposeTheme {
+        Componente01_Box()
+    }
+}
