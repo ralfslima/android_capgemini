ddf8a9d6c685c42eafb30cb63ce54af13b833a7d
ralfslima <62228067+ralfslima@users.noreply.github.com>
2026-07-10 19:14:08 -0300

    10 de julho de 2026

---
+++ Aula 29/projeto1/Projeto1.kt
@@ -0,0 +1,9 @@
+package br.com.treinamento.moduloarquiteturadesoftware.projeto1
+
+import androidx.compose.runtime.Composable
+import br.com.treinamento.moduloarquiteturadesoftware.projeto1.view.ContadorScreen
+
+@Composable
+fun Projeto1() {
+    ContadorScreen()
+}
---
+++ Aula 29/projeto1/model/ContadorUiState.kt
@@ -0,0 +1,3 @@
+package br.com.treinamento.moduloarquiteturadesoftware.projeto1.model
+
+data class ContadorUiState(val contador: Int = 0)
